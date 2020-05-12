6f1602cfc7eb70dca0fdbb81900c6390e2a448b2
github username <github email address>
2020-05-12 15:23:14 -0400

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,9 +4,9 @@
 #   "Amanda" => 27
 # }
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |key, value|
-    if value < 13
-      puts "Happy Birthday #{key}! You are now #{value} years old!"
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
     end
