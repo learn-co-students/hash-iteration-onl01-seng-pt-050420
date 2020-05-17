6a8a101c84ffb798d685783147201cf77ce4731b
travis Lawson <travislawclimb@gmail.com>
2020-05-17 23:08:24 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,18 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |suite,name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
+
+# birthday_kids = {
+#   "Timmy" => 9,
+#   "Sarah" => 6,
+#   "Amanda" => 27
+# }
+
